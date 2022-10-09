d552e434691292bc55a9f3b80154976ce98480f6
Iamvictorsam <iamvictorsam@gmail.com>
2022-10-09 04:27:16 +0100

    update++

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:blog_app/base.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -19,7 +18,7 @@ class _HomeScreenState extends State<HomeScreen> {
   List data = [];
   bool isLoading = true;
 
-  Future retrieve() async {
+  Future retrieveBlogPost() async {
     try {
       final url = Uri.parse("${Base.baseUrl}/v2/posts?auth_token=$apiKey");
 
@@ -41,8 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    retrieve();
-    // print(data.data);
+    retrieveBlogPost();
   }
 
   @override
@@ -50,61 +48,25 @@ class _HomeScreenState extends State<HomeScreen> {
     return isLoading
         ? const Center(child: CircularProgressIndicator())
         : Scaffold(
-            appBar: AppBar(
-              title: const Text('Home'),
-              // actions: [
-              //   IconButton(
-              //       onPressed: () => showSearch(
-              //             context: context,
-              //             delegate: SearchPage(
-              //               onQueryUpdate: print,
-              //               items: data,
-              //               searchLabel: 'Search Blog',
-              //               suggestion: const Center(
-              //                 child: Text('Filter blog by tag'),
-              //               ),
-              //               failure: const Center(
-              //                 child: Text('No blog found :('),
-              //               ),
-              //               filter: (blog) => blog['tags'][0]['name'],
-              //               // sort: (a, b) => a.compareTo(b),
-              //               builder: (blog) => BlogCard(
-              //                 image: data[blog]['featured_image'],
-              //                 title: data[blog]['title'],
-              //                 slug: data[blog]['slug'],
-              //                 desc: data[blog]['summary'],
-              //                 author:
-              //                     "${data[blog]['author']['first_name']} ${data[blog]['author']['last_name']}",
-              //                 authorImg: data[blog]['author']['profile_image'],
-              //                 press: () => Get.to(() => BlogScreen(
-              //                       data: data[blog],
-              //                     )),
-              //               ),
-              //             ),
-              //           ),
-              //       icon: const Icon(Icons.search))
-              // ],
-            ),
+            appBar: AppBar(title: const Text('Home')),
             body: SingleChildScrollView(
                 child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15.0),
               child: Column(children: [
                 const SizedBox(height: 15),
                 ...List.generate(
-                  data.length,
-                  (index) => BlogCard(
-                    image: data[index]['featured_image'],
-                    title: data[index]['title'],
-                    slug: data[index]['slug'],
-                    desc: data[index]['summary'],
-                    author:
-                        "${data[index]['author']['first_name']} ${data[index]['author']['last_name']}",
-                    authorImg: data[index]['author']['profile_image'],
-                    press: () => Get.to(() => BlogScreen(
-                          data: data[index],
-                        )),
-                  ),
-                ),
+                    data.length,
+                    (index) => BlogCard(
+                          image: data[index]['featured_image'],
+                          title: data[index]['title'],
+                          slug: data[index]['slug'],
+                          desc: data[index]['summary'],
+                          author:
+                              "${data[index]['author']['first_name']} ${data[index]['author']['last_name']}",
+                          authorImg: data[index]['author']['profile_image'],
+                          press: () =>
+                              Get.to(() => BlogScreen(data: data[index])),
+                        ))
               ]),
             )),
           );
